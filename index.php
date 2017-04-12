6095e9990a0065905020201b3215ead2b057ff4f
Marcus Lester <mlester3@cnm.edu>
2017-04-12 08:22:21 -0600

    Created a css document

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html>
 		<head>
 			<meta charset="utf-8">
+			<!-- Custom CSS -->
+			<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
 			<title>HTML Sandbox</title>
 		</head>
 		<body>
 <h1>Hi! This is an HTML page :D</h1>
 		</body>
+
 	</html>
+
