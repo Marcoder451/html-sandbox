448de7944212f8e61d3c23a706133db626a3f279
Marcus Lester <mlester3@cnm.edu>
2017-04-12 09:37:45 -0600

    Added table and lists

--- index.php
+++ index.php
@@ -9,8 +9,59 @@
 			<title>HTML Sandbox</title>
 		</head>
 		<body>
-<h1>Hi! This is an HTML page :D</h1>
+			<header>
+				<h1>HTML Sandbox</h1>
+
+				<main>
+					<h2>commander riker says</h2>
+					<p>What's a knock-out like you doing in a computer-generated gin joint like this?</p>
+					<p>Wait a minute - you've been declared dead. You can't give orders around here.</p>
+					<img src="images/cat-trek.png" alt="star trek cat">
+					<p>This is a link to Robbie's favorite site: <a href="http://www.espn.com" target="_blank" >ESPN</a> </p>
+					<p>This is a link to an internal page: <a href="about/about.php">about page</a> </p>
+
+					<h2>A few of my favorite things</h2>
+					<ul>
+						<li>Tacos</li>
+						<li>Kittens</li>
+						<li>Horror Movies</li>
+						<li>Jean-Luc Picard</li>
+					</ul>
+
+					<h2>A few of Sebastion's favorite things</h2>
+					<ol>
+						<li>Spiderman</li>
+						<li>Albuquerque</li>
+						<li>Trolls</li>
+					</ol>
+
+					<h2>Cats &amp; Dogs</h2>
+					<table>
+						<tr>
+							<th>Cats</th>
+							<th>Dogs</th>
+						</tr>
+						<tr>
+							<td>Tabby</td>
+							<td>Bulldog</td>
+						</tr>
+						<tr>
+							<td>Persian</td>
+							<td>Beagle</td>
+						</tr>
+						<tr>
+							<td>Senator</td>
+							<td>Janitor</td>
+						</tr>
+						<tr>
+							<td>Grumpy</td>
+							<td>Happy</td>
+						</tr>
+					</table>
+				</main>
+			</header>
 		</body>
+
 
 	</html>
 
